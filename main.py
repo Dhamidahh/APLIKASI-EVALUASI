7cf666534e37cc277f5a4db5671fb05bbf6fd75a
Dhamidahh <143399574+Dhamidahh@users.noreply.github.com>
2024-01-30 05:09:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -24,13 +24,7 @@ if Nama_Lengkap:
 if NIM:
     st.session_state.age = NIM
 
-SEMESTER_1 = joblib.load('MODEL_SEMESTER1_SVM.pkl')
-SEMESTER_2 = joblib.load('MODEL_SEMESTER2_MLP.pkl')
-SEMESTER_3 = joblib.load('MODEL_SEMESTER3_KNN.pkl')
-SEMESTER_4 = joblib.load('MODEL_SEMESTER4_RF.pkl')
-SEMESTER_5 = joblib.load('MODEL_SEMESTER5_RF.pkl')
-SEMESTER_6 = joblib.load('MODEL_SEMESTER6_XGB.pkl')
-SEMESTER_7 = joblib.load('MODEL_SEMESTER7_RF.pkl')
+SEMESTER_1 = joblib.load('MODEL_SEMESTER1_RF.pkl')
 
 # Sidebar for navigation
 
